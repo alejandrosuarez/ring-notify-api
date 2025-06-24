511ef1d87a2368012b72404d5516c77fc76ddbdc
Alejandro Suarez <git@aspcorpo.com>
2025-06-24 02:54:03 -0500

    fix: enable appDir to support app/api routing in Vercel

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-const nextConfig = { experimental: { serverActions: true } }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    appDir: true
+  }
+}
 
 module.exports = nextConfig
